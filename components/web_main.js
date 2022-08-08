9a40b37fd82013a2f03b80f8e5fbd714e4937087
Lee-Sang-Beom <tkdqja975@naver.com>
2022-08-08 23:48:55 +0900

    test

--- components/web_main.js
+++ components/web_main.js
@@ -78,8 +78,9 @@ export default function WebMain() {
                   DOM
                 </h1>
                 <p class="leading-relaxed mb-3">
-                  DOM이 무엇인지 들어보셨나요? 웹 페이지에서의 DOM은 사용자와의 상호작용과 깊은 관련이 있어요!
-                  이것이 무엇인지, 궁금하다면 아래 Learn 버튼을 눌러주세요!
+                  DOM이 무엇인지 들어보셨나요? 웹 페이지에서의 DOM은 사용자와의
+                  상호작용과 깊은 관련이 있어요! 이것이 무엇인지, 궁금하다면
+                  아래 Learn 버튼을 눌러주세요!
                 </p>
                 <a class="text-indigo-500 inline-flex items-center mt-5">
                   Learn
@@ -134,7 +135,7 @@ export default function WebMain() {
               </div>
             </div>
 
-             {/* box 5*/}
+            {/* box 5*/}
             <div class="p-4 lg:w-1/3">
               <div class="h-full bg-gray-100 bg-opacity-75 dark:bg-gray-800 px-8 pt-14 pb-10 rounded-lg overflow-hidden text-center relative">
                 <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
@@ -165,8 +166,8 @@ export default function WebMain() {
                 </a>
               </div>
             </div>
-                       
-             {/* box 6*/}
+
+            {/* box 6*/}
             <div class="p-4 lg:w-1/3">
               <div class="h-full bg-gray-100 bg-opacity-75 dark:bg-gray-800 px-8 pt-14 pb-10 rounded-lg overflow-hidden text-center relative">
                 <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
@@ -276,8 +277,9 @@ export default function WebMain() {
                   CSR과 SSR
                 </h1>
                 <p class="leading-relaxed mb-3">
-                  DOM이 무엇인지 들어보셨나요? 웹 페이지에서의 DOM은 사용자와의 상호작용과 깊은 관련이 있어요!
-                  이것이 무엇인지, 궁금하다면 아래 Learn 버튼을 눌러주세요!
+                  DOM이 무엇인지 들어보셨나요? 웹 페이지에서의 DOM은 사용자와의
+                  상호작용과 깊은 관련이 있어요! 이것이 무엇인지, 궁금하다면
+                  아래 Learn 버튼을 눌러주세요!
                 </p>
                 <a class="text-indigo-500 inline-flex items-center mt-5">
                   Learn
@@ -375,8 +377,9 @@ export default function WebMain() {
                   {`클로져 (Closure)`}
                 </h1>
                 <p class="leading-relaxed mb-3">
-                  DOM이 무엇인지 들어보셨나요? 웹 페이지에서의 DOM은 사용자와의 상호작용과 깊은 관련이 있어요!
-                  이것이 무엇인지, 궁금하다면 아래 Learn 버튼을 눌러주세요!
+                  DOM이 무엇인지 들어보셨나요? 웹 페이지에서의 DOM은 사용자와의
+                  상호작용과 깊은 관련이 있어요! 이것이 무엇인지, 궁금하다면
+                  아래 Learn 버튼을 눌러주세요!
                 </p>
                 <a class="text-indigo-500 inline-flex items-center mt-5">
                   Learn
@@ -464,18 +467,119 @@ export default function WebMain() {
               </div>
             </div>
 
-            {/* box 12*/}
+            {/* box 15*/}
             <div class="p-4 lg:w-1/3">
               <div class="h-full bg-gray-100 bg-opacity-75 dark:bg-gray-800 px-8 pt-14 pb-10 rounded-lg overflow-hidden text-center relative">
                 <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
                   CATEGORY - WEB BASIC
                 </h2>
                 <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
-                  이벤트 버블링, 캡처링
+                  이벤트 버블링, 이벤트 캡처링
                 </h1>
                 <p class="leading-relaxed mb-3">
-                  DOM이 무엇인지 들어보셨나요? 웹 페이지에서의 DOM은 사용자와의 상호작용과 깊은 관련이 있어요!
-                  이것이 무엇인지, 궁금하다면 아래 Learn 버튼을 눌러주세요!
+                  DOM이 무엇인지 들어보셨나요? 웹 페이지에서의 DOM은 사용자와의
+                  상호작용과 깊은 관련이 있어요! 이것이 무엇인지, 궁금하다면
+                  아래 Learn 버튼을 눌러주세요!
+                </p>
+                <a class="text-indigo-500 inline-flex items-center mt-5">
+                  Learn
+                  <svg
+                    class="w-4 h-4 ml-2"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path d="M5 12h14"></path>
+                    <path d="M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/*여섯 번째 라인*/}
+          <div class="flex flex-wrap -m-4 mt-4">
+            {/* box 16*/}
+            <div class="p-4 lg:w-1/3">
+              <div class="h-full bg-gray-100 bg-opacity-75 dark:bg-gray-800 px-8 pt-14 pb-10 rounded-lg overflow-hidden text-center relative">
+                <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                  CATEGORY : WEB BASIC
+                </h2>
+                <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+                  Webpack, Babel, Polyfill
+                </h1>
+                <p class="leading-relaxed mb-3">
+                  HTML, CSS, JS가 무엇인지 많이 들어보셨나요? 프론트엔드
+                  개발자가 되기 위해서 이것들은 꼭 익혀두셔야 해요! 웹
+                  페이지에서 이것들이 각자 어떠한 역할을 하고 있는지, 궁금하다면
+                  아래 Learn 버튼을 눌러주세요!
+                </p>
+                <a class="text-indigo-500 inline-flex items-center mt-5">
+                  Learn
+                  <svg
+                    class="w-4 h-4 ml-2"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path d="M5 12h14"></path>
+                    <path d="M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+
+            {/* box 17*/}
+            <div class="p-4 lg:w-1/3">
+              <div class="h-full bg-gray-100 bg-opacity-75 dark:bg-gray-800 px-8 pt-14 pb-10 rounded-lg overflow-hidden text-center relative">
+                <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                  CATEGORY : WEB BASIC
+                </h2>
+                <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+                  웹 사이트 성능 최적화
+                </h1>
+                <p class="leading-relaxed mb-3">
+                  웹 페이지에서 새로고침을 통해 페이지를 다시 불러오신 경험이
+                  있나요? 이것과 관련해, 렌더링이라는 용어가 있어요! 렌더링에
+                  대해 잘 모르겠다면, 아래 Learn 버튼을 눌러주세요!
+                </p>
+                <a class="text-indigo-500 inline-flex items-center mt-5">
+                  Learn
+                  <svg
+                    class="w-4 h-4 ml-2"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path d="M5 12h14"></path>
+                    <path d="M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+
+            {/* box 18*/}
+            <div class="p-4 lg:w-1/3">
+              <div class="h-full bg-gray-100 bg-opacity-75 dark:bg-gray-800 px-8 pt-14 pb-10 rounded-lg overflow-hidden text-center relative">
+                <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                  CATEGORY - WEB BASIC
+                </h2>
+                <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+                  실행 컨텍스트
+                </h1>
+                <p class="leading-relaxed mb-3">
+                  DOM이 무엇인지 들어보셨나요? 웹 페이지에서의 DOM은 사용자와의
+                  상호작용과 깊은 관련이 있어요! 이것이 무엇인지, 궁금하다면
+                  아래 Learn 버튼을 눌러주세요!
                 </p>
                 <a class="text-indigo-500 inline-flex items-center mt-5">
                   Learn
