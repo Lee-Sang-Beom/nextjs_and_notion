0b51f4c568575c8ec12186511c718612b9ee0ce4
Lee-Sang-Beom <tkdqja975@naver.com>
2022-08-09 17:57:11 +0900

    fixed build error

--- pages/web_basic/html_css_js.js
+++ pages/web_basic/html_css_js.js
@@ -19,7 +19,7 @@ export default function HTML_CSS_JS() {
           <div className="flex flex-col">
             {/* bar 길이*/}
             <div className="h-1 bg-gray-200 rounded overflow-hidden">
-              <div className="w-24 h-full bg-indigo-500"></div>
+              <div className="w-1/4 h-full bg-indigo-500"></div>
             </div>
             <div className="flex flex-wrap sm:flex-row flex-col py-6 mb-12">
               <h1 className="sm:w-2/5 text-gray-900 font-medium title-font text-2xl mb-2 sm:mb-0">
