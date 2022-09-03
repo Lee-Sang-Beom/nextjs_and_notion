bb61c7e1abc882c3c582d9717fe510f9168e910e
Lee-Sang-Beom <tkdqja975@naver.com>
2022-09-03 21:37:16 +0900

    add template : multi process, thread > in process_and_thread page

--- pages/web_basic/process_and_thread.tsx
+++ pages/web_basic/process_and_thread.tsx
@@ -236,6 +236,107 @@ export default function Process_and_Thread() {
                             </tbody>
                         </table>
                     </div>
+
+                    {/* 내용 4 : 멀티 프로세스와 멀티 스레드 */}
+                    <div className="container px-5 py-12 mx-auto">
+                        {/* bar 길이*/}
+                        <div className="h-1 bg-gray-200 rounded overflow-hidden">
+                            <div className="w-1/2 h-full bg-indigo-500"></div>
+                        </div>
+
+                        {/* 멀티 프로세스 */}
+                        <div className="mb-12">
+                            <h1 className="text-3xl font-bold title-font text-gray-900 mt-6 mb-2 ">Multi Process</h1>
+                            <p className="sm:w-full leading-relaxed text-base sm:pl-10 pl-0 mt-1 mb-6">
+                                {`- 멀티프로세스는 하나의 응용프로그램을 여러 개의 프로세스로 구성하여 각 프로세스가 하나의 작업(태스크)을 처리하도록 하는 것이에요.`}
+                            </p>
+                            <div className="flex flex-wrap -m-4">
+                                <div className="p-4 md:w-1/2 w-full">
+                                    <div className="h-full bg-gray-100 dark:bg-gray-200 p-8 rounded">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">
+                                            <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+                                        </svg>
+                                        <p className="leading-relaxed mb-3 dark:text-slate-700">
+                                            {``}
+                                            </p>
+                                        <p className="leading-relaxed mb-3 dark:text-slate-700">
+                                            {``}
+                                        </p>
+                                        <p className="leading-relaxed dark:text-slate-700">
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="p-4 md:w-1/2 w-full">
+                                    <div className="h-full bg-gray-100 dark:bg-gray-200 p-8 rounded">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">
+                                            <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+                                        </svg>
+                                        <p className="leading-relaxed mb-3 dark:text-slate-700">
+                                            {``}
+                                            </p>
+                                        <p className="leading-relaxed mb-3 dark:text-slate-700">
+                                            {``}
+                                        </p>
+                                        <p className="leading-relaxed mb-3 dark:text-slate-700">
+                                            {``}
+                                        </p>
+                                        <p className="leading-relaxed mb-3 dark:text-slate-700">
+                                            {``}
+                                        </p>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                        </div>
+
+                        {/* 멀티 스레드 */}
+                        <div className="mb-12">
+                            <h1 className="text-3xl font-bold title-font text-gray-900 mt-6 mb-2 ">Multi Thread</h1>
+                            <p className="sm:w-full leading-relaxed text-base sm:pl-10 pl-0 mt-1 mb-6">
+                                {`- 멀티스레드는 하나의 응용프로그램을 여러 개의 스레드로 구성하고, 각 스레드로 하여금 하나의 작업을 처리하도록 하는 것이에요.`}
+                            </p>
+                            <div className="flex flex-wrap -m-4">
+                                <div className="p-4 md:w-1/2 w-full">
+                                    <div className="h-full bg-gray-100 dark:bg-gray-200 p-8 rounded">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">
+                                            <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+                                        </svg>
+                                        <p className="leading-relaxed mb-3 dark:text-slate-700">
+                                            {``}
+                                            </p>
+                                        <p className="leading-relaxed mb-3 dark:text-slate-700">
+                                            {``}
+                                        </p>
+                                        <p className="leading-relaxed dark:text-slate-700">
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="p-4 md:w-1/2 w-full">
+                                    <div className="h-full bg-gray-100 dark:bg-gray-200 p-8 rounded">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">
+                                            <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+                                        </svg>
+                                        <p className="leading-relaxed mb-3 dark:text-slate-700">
+                                            {``}
+                                            </p>
+                                        <p className="leading-relaxed mb-3 dark:text-slate-700">
+                                            {``}
+                                        </p>
+                                        <p className="leading-relaxed mb-3 dark:text-slate-700">
+                                            {``}
+                                        </p>
+                                        <p className="leading-relaxed mb-3 dark:text-slate-700">
+                                            {``}
+                                        </p>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                        </div>
+                   
+                   
+                       
+                    </div>
                 </section>
 
             </Layout>
