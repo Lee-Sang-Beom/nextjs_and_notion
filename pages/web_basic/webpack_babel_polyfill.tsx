629bce276ffba2c160833074132fbd7e9b6a96e3
Lee-Sang-Beom <tkdqja975@naver.com>
2022-09-26 00:56:27 +0900

    add content : webpack(2)

--- pages/web_basic/webpack_babel_polyfill.tsx
+++ pages/web_basic/webpack_babel_polyfill.tsx
@@ -98,7 +98,7 @@ export default function Webpack_Babel_Polyfill() {
                         </div>
                         <div className="w-full flex flex-wrap">
                             <div className="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden flex flex-col">
-                                <img alt="feature" className="object-center h-full w-full" src="/img/webpack.jpg"/>
+                                <img alt="feature" className="object-center h-full w-full" src="/img/webpack.jpg" />
                                 <a
                                     className="text-sm text-center pt-1"
                                     title="이동하기"
@@ -107,22 +107,35 @@ export default function Webpack_Babel_Polyfill() {
                                     {`이미지 출처 : Khaydarov Murod님의 포스트(H-web)`}
                                 </a>
                             </div>
-                            
-                            <div className="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 text-left">
+
+                            <div className="flex flex-col flex-wrap py-3 lg:py-0 -mb-10 lg:w-1/2 lg:pl-12 text-left">
                                 <div className="flex flex-col mb-4 lg:items-start items-center">
-                                    <p className="leading-relaxed text-base">{`코드의 재사용/유지보수를 위해 파일을 분리한 것을 `}
-                                    <strong>모듈</strong>
-                                    {`이라고 해요. 자주 사용하는 코드는 모듈로 분리하는 것이 좋다는 것 정도는 알고 계실거에요. 하지만, 개발 편의성을 위해 모듈을 너무 많이 만들면 어떻게 될까요?`}</p>
+                                    <p className="leading-relaxed text-base">
+                                        {`위의 문제를 해결하기 위해, 서버로 요청하는 HTTP 개수가 감소되도록, 
+                                        개발할 때는 모듈로 나눠 개발하고 
+                                        웹 서버에 배포할 때는 하나의 파일로 묶어 배포하는 방법을 생각할 수 있어요.`}
+                                    </p>
                                 </div>
-                                <div className="flex flex-col mb-2 lg:items-start items-center">
-                                    <p className="leading-relaxed text-base">{`저희가 웹 애플리케이션을 만들고, 사용자가 저희의 웹 사이트를 보려할 때 내부적으로 아래와 같은 과정이 발생해요.`}</p>
+                                <div className="flex flex-col mb-4 lg:items-start items-center">
+                                    <p className="leading-relaxed text-base">
+                                        {`여기서, 여러 모듈을 하나로 묶는 작업을 `}
+                                        <strong>{`번들(Bundle)`}</strong>
+                                        {`이라고 하고, 묶어주는 도구를 `}
+                                        <strong>{`모듈 번들러(Module Bundler)`}</strong>
+                                        {`라고 해요. 
+                                        Webpack은 이 모듈 번들러의 하나에요.`}</p>
                                 </div>
-                                <div className="leading-relaxed text-base mb-8">
-                                    <ul className="list-disc ml-7 text-sm text-slate-500 dark:text-slate-300 ">
-                                        <li className="mb-1 list-decimal">{`웹 애플리케이션 완성 시, 웹 서버에 HTML, CSS, JS 파일을 배포해요`}</li>
-                                        <li className="mb-1 list-decimal">{`사용자가 브라우저를 통해 웹 사이트에 접근하면, 브라우저는 사용자에게 웹 사이트를 보여주기 위해 웹 서버에 HTML, CSS, JS 등의 리소스를 요청해요.`}</li>
-                                        <li className="mb-1 list-decimal">{`웹 서버는 리소스를 포함해 응답해요. 이 과정이 끝나면 비로소 사용자는 웹 사이트를 볼 수 있게 되요.`}</li>
-                                    </ul>
+                                <div className="flex flex-col lg:items-start items-center">
+                                    <p className="leading-relaxed text-base">
+                                        <strong>{`즉, Webpack은 여러 파일을 하나의 파일로 묶는 모듈 번들러 중 하나라고 정리할 수 있어요. `}</strong>
+                                    {`모듈 번들러는 프론트엔드 측에서 서버로 HTTP 요청 시, 요청 개수를 줄여줌으로써 `}
+                                    <strong>성능을 향상</strong>
+                                    {`시키고, 공백 등을 없애므로 `}
+                                    <strong>리소스</strong>
+                                    {`를 최적화시켜줘요. 이와 같은 이유로 사용자 경험이 좋아져요. 더 궁금한 내용이 있다면 `}
+                                    <a href="https://joshua1988.github.io/webpack-guide/">{`"여기"`}</a>
+                                    {`를 클릭해주세요. 추천하는 포스트에요!`}
+                                    </p>
                                 </div>
                             </div>
                         </div>
