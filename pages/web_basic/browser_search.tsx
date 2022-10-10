dd394644d6542092997ed41080884d98a440c024
Lee-Sang-Beom <tkdqja975@naver.com>
2022-10-10 16:38:06 +0900

    fixed error: browser_search page->rendering page link

--- pages/web_basic/browser_search.tsx
+++ pages/web_basic/browser_search.tsx
@@ -354,7 +354,7 @@ export default function Browser_search() {
                                         </p>
                                         <p className="leading-relaxed">
                                             {`이전에 배웠던 렌더링 과정이 기억나시나요? 지금부터 그 과정이 시작되는거에요. 잘 모르겠다면 `}
-                                            <a href="http://localhost:3000/web_basic/rendering">{`"여기"`}</a>
+                                            <a href="https://nextjs-and-notion-vercel-79a3rrkpg-lee-sang-beom.vercel.app/web_basic/rendering">{`"여기"`}</a>
                                             {`를 클릭해주세요.`}
                                         </p>
                                     </div>
