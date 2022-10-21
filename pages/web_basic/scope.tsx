f620117b5eb1fc04247b0dd7f7cc5411d7a5cce0
Lee-Sang-Beom <tkdqja975@naver.com>
2022-10-21 14:05:13 +0900

    notify design : divide screen > blockLevelScope mediaquery in scope page

--- pages/web_basic/scope.tsx
+++ pages/web_basic/scope.tsx
@@ -64,7 +64,7 @@ export default function Scope() {
                                 {`- JavaScript에서의 스코프는 참조 범위에 따라, 아래와 같이 2가지로 분류할 수 있어요.`}
                             </p>
                             <div className="flex flex-wrap -m-4">
-                                <div className="p-4 md:w-1/2 w-full">
+                                <div className="p-4 lg:w-1/2 w-full">
                                     <div className="h-full bg-gray-100 dark:bg-gray-200 p-8 rounded">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">
                                             <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
@@ -75,10 +75,10 @@ export default function Scope() {
                                         <strong>지역 스코프</strong>{`라는 것도 있어요.`}</p>  
                                     </div>
                                 </div>
-                                <div className="p-4 md:w-1/2 w-full">
+                                <div className="p-4 lg:w-1/2 w-full">
                                     <img
                                         alt="content"
-                                        className="object-cover object-center mx-auto"
+                                        className="object-cover object-center mx-auto rounded"
                                         src="/img/globalScope.jpg"
                                     />
                                 </div>
@@ -89,7 +89,7 @@ export default function Scope() {
                         <div className="mb-12">
                             <h1 className="text-2xl font-bold title-font text-gray-900 mt-6 mb-5 ">함수 레벨 스코프</h1>
                             <div className="flex flex-wrap -m-4">
-                                <div className="p-4 md:w-1/3 w-full">
+                                <div className="p-4 lg:w-full xl:w-1/3 w-full">
                                     <div className="h-full bg-gray-100 dark:bg-gray-200 p-8 rounded">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">
                                             <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
@@ -102,25 +102,27 @@ export default function Scope() {
                                             <strong>{`함수 레벨 스코프란 `}</strong>
                                             {`함수 코드 블록 내에서 선언된 변수는 함수 코드 블록 내에서만 유효하고 함수 외부에서는 유효하지 않은 스코프를 의미해요.`}
                                         </p>
-                                        <p className="leading-relaxed  dark:text-slate-700">
+                                        <p className="leading-relaxed dark:text-slate-700">
                                             {`다음 이미지 예제의 코드와 주석 부분을 잘 확인해보시면 이해가 쉬울 거에요.`}
                                         </p>
                                     </div>
                                 </div>
-                                <div className="p-4 md:w-1/3 w-full">
+
+                                <div className="p-4 lg:w-1/2 xl:w-1/3 w-full">
                                     <img
                                         className="object-cover object-center rounded mb-1"
                                         alt="hero"
                                         src="/img/funcLevelScope1.jpg"
                                     />
                                 </div>
-                                <div className="p-4 md:w-1/3 w-full h-full">
+                                <div className="p-4 lg:w-1/2 xl:w-1/3 w-full">
                                     <img
                                         className="object-cover object-center rounded mb-1"
                                         alt="hero"
                                         src="/img/funcLevelScope2.jpg"
                                     />
                                 </div>
+
                             </div>
                         </div>
 
