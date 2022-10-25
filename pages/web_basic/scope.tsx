455ea28b61930d6dbfac8f99257440eb7292d88e
Lee-Sang-Beom <tkdqja975@naver.com>
2022-10-25 20:02:34 +0900

    notify annotation

--- pages/web_basic/scope.tsx
+++ pages/web_basic/scope.tsx
@@ -54,7 +54,6 @@ export default function Scope() {
                             <div className="w-2/3 h-full bg-indigo-500"></div>
                         </div>
 
-                        {/* 분류 */}
                         <div className="mb-12">
                             <h1 className="text-2xl font-bold title-font text-gray-900 mt-6 mb-2 ">전역 스코프와 지역 스코프</h1>
                             <p className="sm:w-full leading-relaxed text-base sm:pl-10 pl-0 mt-1">
@@ -162,17 +161,15 @@ export default function Scope() {
                         
                     </div>
 
-                    {/* 내용 2 : 스코프 분류 */}
+                    {/* 내용 3 : 스코프 체인 */}
                     <div className="container px-5 py-12 mx-auto">
                         {/* bar 길이*/}
                         <div className="h-1 bg-gray-200 rounded overflow-hidden">
                             <div className="w-full h-full bg-indigo-500"></div>
                         </div>
 
-                        {/* 분류 */}
                         <div className="mb-12">
-                            <h1 className="text-2xl font-bold title-font text-gray-900 mt-6 mb-2 ">스코프 체인</h1>
-        
+                            <h1 className="text-2xl font-bold title-font text-gray-900 mt-6 mb-4">스코프 체인</h1>
                             <div className="flex flex-wrap -m-4">
                                 <div className="p-4 lg:w-1/2 w-full">
                                     <div className="h-full bg-gray-100 dark:bg-gray-200 p-8 rounded">
@@ -180,8 +177,9 @@ export default function Scope() {
                                             <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
                                         </svg>
                                         <p className="leading-relaxed mb-1 dark:text-slate-700"><strong>JavaScript 엔진</strong>{`은 식별자를 찾을 때 우선, 일단 자신이 속한 스코프에서 찾아요.`}</p>  
-                                        <p className="leading-relaxed mb-1 dark:text-slate-700">{`이 때, 해당 스코프에 식별자가 없으면, 해당 스코프를 포함하는 상위 스코프로 이동해 식별자를 찾아나가요.`}</p>  
+                                        <p className="leading-relaxed mb-1 dark:text-slate-700">{`이 때, 해당 스코프에 식별자가 없으면, 해당 스코프를 포함하는 `}<strong>상위 스코프</strong>{`로 이동해 식별자를 찾아나가요.`}</p>  
                                         <p className="leading-relaxed mb-1 dark:text-slate-700">{`이렇게 연속적으로 연결고리를 타면서 식별자를 찾아나가는 과정을 `}<strong>스코프 체인</strong>{`이라고 해요.`}</p>  
+                                        <p className="leading-relaxed dark:text-slate-700">{`다음 이미지 예제의 코드와 주석 부분을 잘 확인해보시면 이해가 쉬울 거에요.`} </p>
                                     </div>
                                 </div>
                                 <div className="p-4 lg:w-1/2 w-full">
@@ -193,8 +191,6 @@ export default function Scope() {
                                 </div>
                             </div>
                         </div>
-
-   
                     </div>
                 </section>
             </Layout>
